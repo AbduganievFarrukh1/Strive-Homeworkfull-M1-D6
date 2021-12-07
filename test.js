8a5fff3a4acc2f518bea380a4c217c38531fcbb5
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-08 00:21:42 +0100

    next-task failed

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-const tree = function (height) {
-    for (let i = 0; i < height; i++) {
-      let stars = "*".repeat(2 * i + 1);
-      let spacesBefore = " ".repeat(height - i - 1);
-      console.log(spacesBefore + stars);
+// const tree = function (height) {
+//     for (let i = 0; i < height; i++) {
+//       let stars = "*".repeat(2 * i + 1);
+//       let spacesBefore = " ".repeat(height - i - 1);
+//       console.log(spacesBefore + stars);
+//     }
+//   };
+//   tree(10)
+
+
+  //-------next task-------//
+  const sumAllTheYears = function () {
+    let result = 0;
+    for (let i = 0; i < movies.length; i++) {
+      result += parseInt(movies[i].Year);
     }
-  };
-  tree(10)
+    return result;
+  }
+  console.log(rsumAllTheYear);
+  
